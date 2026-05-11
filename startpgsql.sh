af756c5f23d41650a389c84fd064c0910cc81e13
bahaelaila7 <bahaelaila7@gmail.com>
2026-05-11 17:28:39 -0400

    Update startpgsql.sh
    change socket location to same location

--- startpgsql.sh
+++ startpgsql.sh
@@ -5,5 +5,6 @@
 if [ ! -d "${DBDIR}" ]; then
 	echo "INITIALIZING DB"
 	initdb -D "${DBDIR}"
+	sed -i 's|#unix_socket_directories =.*$|unix_socket_directories = '"'.'"'|' "${DBDIR}/postgresql.conf" 
 fi
 pg_ctl -D "${DBDIR}" -l pglogfile start -o "-p $PORT"
