1c566fb21de05f0bc0d6356ce577af6ad36a19a2
bahaelaila7 <bahaelaila7@gmail.com>
2026-05-11 17:44:34 -0400

    Update startpgsql.sh

--- startpgsql.sh
+++ startpgsql.sh
@@ -5,6 +5,6 @@
 if [ ! -d "${DBDIR}" ]; then
 	echo "INITIALIZING DB"
 	initdb -D "${DBDIR}"
-	sed -i 's|#unix_socket_directories =.*$|unix_socket_directories = '"'.'"'|' "${DBDIR}/postgresql.conf" 
+	sed -i 's|#unix_socket_directories =.*$|unix_socket_directories = '"'$DBDIR'"'|' "${DBDIR}/postgresql.conf" 
 fi
 pg_ctl -D "${DBDIR}" -l pglogfile start -o "-p $PORT"
